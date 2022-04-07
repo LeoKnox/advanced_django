874c37530f7ce44400da4beaf71c1062ebca9505
LeoKnox <LeoKnox@users.noreply.github.com>
2022-04-07 11:43:41 -0700

    Create bad_word_validator.py

---
+++ bad_word_validator.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import ValidationError
+
+BAD_WORDS = ['star wars', 'election', 'tomato']
+def validate_no_bad_words(content):
+  if any([word in content.lower() for word in BAD_WORDS]):
+    raise ValidationError('This post contains bad words!')
+ 
+class Post(models.Model):
+  author = models.ForeignKey(User, null=True, blank=True, on_delete=CASCADE)
+  content = models.CharField(max_length=140, validators=[validate_no_bad_words])
+  created_on models.DateTimeField('date created', auto_now_add=True)
+  
+  def __str__(self):
+  return '{}: {}'.format(self.author, self.content[0:20])
